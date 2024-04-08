7681a72204ceaefcf13c6d4f990bc74ed1161ee0
Sukanya Yanamala <ysukanya383@gmail.com>
2024-04-08 11:56:42 +0100

    Initial Commit

---
+++ Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Sukanya Yanamala on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
